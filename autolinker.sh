69f8ad439b19709249dcf1c096ce2ea68da65d00
Samarth Patel <samarthxpatel@gmail.com>
2020-09-23 18:17:57 -0400

    Automatically link demo app modules to repo

---
+++ autolinker.sh
@@ -0,0 +1,22 @@
+read -p 'Path to demo app: ' dirpath
+
+pushd $dirpath/node_modules/react
+yarn link
+popd
+
+pushd $dirpath/node_modules/react-dom
+yarn link
+popd
+
+pushd $dirpath/node_modules/firebase
+yarn link
+popd
+
+pushd $dirpath/node_modules/reactfire
+yarn link
+popd
+
+yarn link react
+yarn link react-dom
+yarn link firebase
+yarn link reactfire
