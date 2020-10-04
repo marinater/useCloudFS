40aa8d858754fe03aa4f08e0f68e5d75689ea645
Omar Cobas <omarlcobas@gmail.com>
2020-10-03 21:14:18 -0400

    working on renameFile lambda function with graphql

---
+++ src/graphql/queries.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const renameFile = /* GraphQL */ `
+  query RenameFile($oldName: String, $newName: String) {
+    renameFile(oldName: $oldName, newName: $newName)
+  }
+`;
