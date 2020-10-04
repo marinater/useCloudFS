477df26c6f1c2d6ca4d3da5edec619348fcda74d
Samarth Patel <samarthxpatel@gmail.com>
2020-10-04 17:27:23 -0400

    Changed autolinker to will always link firebase since it is for development only

--- autolink.sh
+++ autolink.sh
@@ -2,33 +2,43 @@
 
 # read -p 'Path to demo app: ' dirpath
 dirpath='./demo'
-echo $dirpath
-yarn link
 
+# Make the local usecloudfs available for other use
+yarn link
+# Add usecloudfs to the demo app
+pushd $dirpath
+yarn link usecloudfs
+popd
+
+# Make the demo app's react package available for use
 pushd $dirpath/node_modules/react
 yarn link
 popd
+# Add react to usecloudfs
+yarn link react
 
+# Make the demo app's react-dom package available for use
 pushd $dirpath/node_modules/react-dom
 yarn link
 popd
+# Add react-dom to usecloudfs
+yarn link react-dom
 
-if [ -d $dirpath/node_modules/firebase ]; then
+# # Autolink firebase only if demo app uses it
+# if [ -d $dirpath/node_modules/firebase ]; then
+    # Make the demo app's firebase package available for use
     pushd $dirpath/node_modules/firebase
     yarn link
     popd
-fi
-if [ -d $dirpath/node_modules/reactfire ]; then
+    # Add firebase to usecloudfs
+    yarn link firebase
+# fi
+# Autolink reactfire only if demo app uses it
+# if [ -d $dirpath/node_modules/reactfire ]; then
+    # Make the demo app's reactfire package available for use
     pushd $dirpath/node_modules/reactfire
     yarn link
     popd
-fi
-
-yarn link react
-yarn link react-dom
-if [ -d $dirpath/node_modules/firebase ]; then
-    yarn link firebase
-fi
-if [ -d $dirpath/node_modules/reactfire ]; then
+    # Add reactfire to usecloudfs
     yarn link reactfire
-fi
+# fi
--- autounlink.sh
+++ autounlink.sh
@@ -3,21 +3,42 @@
 # read -p 'Path to demo app: ' dirpath
 dirpath='./demo'
 
+# Remove usecloudfs from demo app
 pushd $dirpath
 yarn unlink usecloudfs
 popd
-
-yarn unlink react
-yarn unlink react-dom
+# Make the local usecloudfs package unavailable for use
 yarn unlink
 
+# Remove usecloudfs's use of react
+yarn unlink react
+# Make local react unavailable for use
+pushd $dirpath/node_modules/react
+yarn unlink
+popd
+
+# Remove usecloudfs's use of react-dom
+yarn unlink react-dom
+# Make local react-dom unavailable for use
+pushd $dirpath/node_modules/react-dom
+yarn unlink
+popd
+
+# Autounlink firebase only if demo app uses it
 if [ -d $dirpath/node_modules/firebase ]; then
-    pushd $dirpath/node_modules/firebase
-    yarn unlink
-    popd
+	# Remove usecloudfs's use of firebase
+	yarn unlink firebase
+	# Make local firebase unavailable for use
+	pushd $dirpath/node_modules/firebase
+	yarn unlink
+	popd
 fi
+# Autounlink reactfire only if demo app uses it
 if [ -d $dirpath/node_modules/reactfire ]; then
-    pushd $dirpath/node_modules/reactfire
-    yarn unlink
-    popd
+	# Remove usecloudfs's use of reactfire
+	yarn unlink reactfire
+	# Make local reactfire unavailable for use
+	pushd $dirpath/node_modules/reactfire
+	yarn unlink
+	popd
 fi
