044194c7e45c7a1eb746ff69d608474022f56c94
Omar Cobas <41080197+omarASC5@users.noreply.github.com>
2020-10-09 18:37:13 -0400

    renameFolder S3 Function -> put on lambda

---
+++ aws_functions/renameFolder.py
@@ -0,0 +1,19 @@
+import boto3
+
+s3 = boto3.resource('s3')
+
+# Setup, get name of bucket
+s3BucketName = 'amplify-usecloudfs-dev-182044-deployment'
+bucket = s3.Bucket(s3BucketName)
+
+def renameFolder(oldName, newName):
+	for object in bucket.objects.filter(Prefix=oldName):
+			srcKey = object.key
+			if not srcKey.endswith('/'):
+					fileName = srcKey.split('/')[-1]
+					destFileKey = newName + '/' + fileName
+					copySource = s3BucketName + '/' + srcKey         
+					s3.Object(s3BucketName, destFileKey).copy_from(CopySource=copySource)
+					s3.Object(s3BucketName, srcKey).delete()
+
+renameFolder('brahhhh', 'bruh')
