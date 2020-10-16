9a2f4afd18d23e30ae3845cc9ca669182a043191
molly34672 <meixinliang8@gmail.com>
2020-10-16 17:59:07 -0400

    Added prebuilt UI components for user login and initialized buttons for testing queries

--- amplify-demo/src/App.tsx
+++ amplify-demo/src/App.tsx
@@ -1,77 +1,87 @@
 
-import React, { } from "react";
-//import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import React, { Component } from "react";
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 // import { useCloudFS } from 'usecloudfs';
 import './App.css';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import Amplify from 'aws-amplify';
 import awsconfig from './aws-exports';
 
-//import * as queries from './graphql/queries';
+import * as queries from './graphql/queries';
 // import * as mutations from './graphql/mutations';
 // import * as subscriptions from './graphql/subscriptions';
-//import { API } from 'aws-amplify';
+import { API } from 'aws-amplify';
 Amplify.configure(awsconfig);
 
 
-// class App extends Component {
-//     // async function signIn() {
-//     //     try {
-//     //         const user = await Auth.signIn("test-email", "test-password");
-//     //     } catch (error) {
-//     //         console.log('error signing in', error);
-//     //     }
-//     // }
+class App extends Component {
+    // async function signIn() {
+    //     try {
+    //         const user = await Auth.signIn("test-email", "test-password");
+    //     } catch (error) {
+    //         console.log('error signing in', error);
+    //     }
+    // }
 
-//     listBuckets = async () => {
-//         console.log('listing buckets');
-//         try {
-//             const buckets = await API.graphql({ query: queries.listBuckets });
-//             alert(JSON.stringify(buckets))
-//         } catch (error) {
-//             console.log('error listing buckets', error);
-//         }
-//     };
-//     render() {
-//         return (
-//             <div className="App">
-//                 <AmplifySignOut />
-//                 <p> Click a button </p>
-//                 <button onClick={this.listBuckets}>GraphQL Query</button>
-//             </div>
-//         );
-//     }
+    listBuckets = async () => {
+        console.log('listing buckets');
+        try {
+            const buckets = await API.graphql({ query: queries.listBuckets });
+            alert(JSON.stringify(buckets))
+        } catch (error) {
+            console.log('error listing buckets', error);
+        }
+    };
+
+    getBuckets = async () => {
+        console.log('getting buckets');
+        try {
+            const buckets = await API.graphql({ query: queries.getBucket, variables: { id: '1' } });
+            alert(JSON.stringify(buckets))
+        } catch (error) {
+            console.log('error getting buckets', error);
+        }
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <AmplifySignOut />
+                <p> Click a button </p>
+                <button onClick={this.listBuckets}>List Buckets</button>
+                <button onClick={this.getBuckets}>Get Bucket</button>
+            </div>
+        );
+    }
+}
+
+export default withAuthenticator(App);
+
+// function App() {
+//     const x = useCloudFS
+
+//      console.log(x)
+
+
+//     return (
+//         <div className="App">
+//             <header className="App-header">
+//                 <img src={logo} className="App-logo" alt="logo" />
+//                 <p>
+//                     Edit <code>src/App.tsx</code> and save to reload.
+//                     </p>
+//                 <a
+//                     className="App-link"
+//                     href="https://reactjs.org"
+//                     target="_blank"
+//                     rel="noopener noreferrer"
+//                 >
+//                     Learn React
+//                     </a>
+//             </header>
+
+//         </div>
+//     );
 // }
 
 
-function App() {
-    // const x = useCloudFS
-
-    // console.log(x)
-
-
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                    </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                    </a>
-            </header>
-
-        </div>
-    );
-}
-
-
-
-
-export default App;
